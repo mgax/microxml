0dc58bc104388b30c5f168f2712939ce2aed37c6
Alex Morega <alex@grep.ro>
2011-06-11 10:44:03 +0300

    reverse special treatment of xmlns attributes

--- tests/test_microlark_suite.py
+++ tests/test_microlark_suite.py
@@ -13,22 +13,39 @@ def test_well_formed():
 
 
 def handle_events(event_iter):
+    root_ns = None
     for element in event_iter:
-        yield 'tag', element.tag
-        for name, value in element.attrib.iteritems():
-            yield 'attrib', name, value
+        nsmap = getattr(element, 'nsmap', None) or {}
+        if None in nsmap:
+            root_ns = nsmap[None]
+
+        tag = element.tag
+        if root_ns is not None:
+            prefix = '{%s}' % root_ns
+            assert tag.startswith(prefix)
+            tag = tag[len(prefix):]
+        yield 'tag', tag
+
+        attrib = dict(element.attrib)
+        for ns_name in nsmap:
+            attr_name = 'xmlns'
+            if ns_name is not None:
+                attr_name += ':' + ns_name
+            attrib[attr_name] = nsmap[ns_name]
+        for attr_name in sorted(attrib):
+            yield 'attrib', attr_name, attrib[attr_name]
 
 
 def check_well_formed(case_file):
     data = case_file.read()
-    etree_events = list(handle_events(lxml.etree.fromstring(data).iter("*")))
     microxml_events = list(handle_events(microxml.fromstring(data)))
+    etree_events = list(handle_events(lxml.etree.fromstring(data).iter("*")))
     try:
-        assert etree_events == microxml_events
+        assert microxml_events == etree_events
     except:
         print "file:", str(case_file), repr(case_file.read())
-        print "etree output:"
-        pprint.pprint(etree_events)
         print "microxml output:"
         pprint.pprint(microxml_events)
+        print "etree output:"
+        pprint.pprint(etree_events)
         raise
